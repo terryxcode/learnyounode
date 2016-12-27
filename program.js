99bdda360249c476fe491a8e16df9a745ff6accf
terryx <terry.xu@ehealth-china.com>
2016-12-27 14:43:52 +0800

    MY FIRST ASYNC I/O

--- program.js
+++ program.js
@@ -1,3 +1,8 @@
 let fs = require('fs');
-let lines = fs.readFileSync(process.argv[2]).toString().split('\n').length - 1;
-console.log(lines);
+fs.readFile(process.argv[2], (err, data) => {
+    if (err) {
+        return console.error(err);
+    }
+    let lines = data.toString().split('\n').length - 1;
+    console.log(lines);
+});
