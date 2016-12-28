c8f5e8dd4aea6f1218deabb5a21c46a7be3f946d
terryx <terry.xu@ehealth-china.com>
2016-12-28 13:38:12 +0800

    TIME SERVER

--- program.js
+++ program.js
@@ -1,25 +1,7 @@
-let http = require('http');
-let bl = require('bl');
+let net = require('net');
+let time = require('strftime');
 
-let count = 3;
-let contents = [];
-for (let i = 0; i < 3; i++) {
-    const k = i;
-    http.get(process.argv[k + 2],
-        res => {
-            res.pipe(bl(
-                (err, data) => {
-                    if (err) {
-                        console.error(err);
-                    }
-                    contents[k] = data.toString();
-                    --count;
-
-                    if (count === 0) {
-                        contents.forEach(c => console.log(c));
-                    }
-                }
-            ));
-        }
-    ).on('error', console.error);
-}
+net.createServer(s => {
+    let data = time('%F %H:%M', new Date());
+    s.end(data + '\n');
+}).listen(process.argv[2]);
