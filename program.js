a494eb5c1d8631ed7d1dc195c675195a81919163
terryx <terry.xu@ehealth-china.com>
2016-12-28 13:52:43 +0800

    HTTP UPPERCASERER

--- program.js
+++ program.js
@@ -1,6 +1,13 @@
 let http = require('http');
 let fs = require('fs');
+let map = require('through2-map');
 
 http.createServer((req, res) => {
-    fs.createReadStream(process.argv[3]).pipe(res);
-}).listen(process.argv[2]);
+    if (req.method === 'POST') {
+        req.pipe(map(chunk => {
+            return chunk.toString().toUpperCase();
+        })).pipe(res);
+    } else {
+        return res.end('send me a POST\n');
+    }
+}).listen(Number(process.argv[2]));
