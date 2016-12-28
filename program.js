c66c4ba0c8622778bdfd824d3b72c6cd5cae415c
terryx <terry.xu@ehealth-china.com>
2016-12-28 12:33:49 +0800

    JUGGLING ASYNC

--- program.js
+++ program.js
@@ -1,18 +1,25 @@
 let http = require('http');
 let bl = require('bl');
 
-http.get(process.argv[2],
-    res => {
-        res.setEncoding('utf8');
-        res.pipe(bl(
-            (err, data) => {
-                if (err) {
-                    console.error(err);
+let count = 3;
+let contents = [];
+for (let i = 0; i < 3; i++) {
+    const k = i;
+    http.get(process.argv[k + 2],
+        res => {
+            res.pipe(bl(
+                (err, data) => {
+                    if (err) {
+                        console.error(err);
+                    }
+                    contents[k] = data.toString();
+                    --count;
+
+                    if (count === 0) {
+                        contents.forEach(c => console.log(c));
+                    }
                 }
-                data = data.toString();
-                console.log(data.length);
-                console.log(data);
-            }
             ));
-    }
-).on('error', console.error);
+        }
+    ).on('error', console.error);
+}
