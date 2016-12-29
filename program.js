c1f350c8a6f641f1f4e7bed97eed2e7fd619821c
terryx <terry.xu@ehealth-china.com>
2016-12-29 22:16:34 +0800

    HTTP JSON API SERVER

--- program.js
+++ program.js
@@ -1,13 +1,41 @@
 let http = require('http');
-let fs = require('fs');
-let map = require('through2-map');
+let url = require('url');
 
 http.createServer((req, res) => {
-    if (req.method === 'POST') {
-        req.pipe(map(chunk => {
-            return chunk.toString().toUpperCase();
-        })).pipe(res);
+    if (req.method === 'GET') {
+        let reqUrl = url.parse(req.url, true);
+        let iso = new Date(reqUrl.query.iso);
+        let result;
+        switch (reqUrl.pathname) {
+            case '/api/parsetime':
+                result = parsetime(iso);
+                break;
+            case '/api/unixtime':
+                result = unixtime(iso);
+                break;
+        }
+        if (result) {
+            // res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.end(JSON.stringify(result));
+        } else {
+            res.writeHead(404);
+            res.end();
+        }
     } else {
-        return res.end('send me a POST\n');
+        res.end('please send a get request');
     }
 }).listen(Number(process.argv[2]));
+
+function parsetime(date) {
+    return {
+        "hour": date.getHours(),
+        "minute": date.getMinutes(),
+        "second": date.getSeconds()
+    }
+}
+
+function unixtime(date) {
+    return {
+        "unixtime": date.getTime()
+    }
+}
